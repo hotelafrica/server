dea0180715efabbdfb7518baf667d207365f6525
hotelafrica <34492804+hotelafrica@users.noreply.github.com>
2025-02-08 22:18:21 -0500

    Update 50_update_linux.sh

--- cmd_via_homeassistant/50_update_linux.sh
+++ cmd_via_homeassistant/50_update_linux.sh
@@ -3,4 +3,4 @@ apt update
 apt full-upgrade -y
 apt autoremove -y
 
-echo "$(date +'%Y/%m/%d') - $(date +'%r') : (Log) sys update done." >> /opt/shared_storage/server_sys_log.txt
+echo "$(date +'%Y/%m/%d') - $(date +'%r') : (Log) sys update done." >> /opt/XXX/XXX/server_sys_log.txt
